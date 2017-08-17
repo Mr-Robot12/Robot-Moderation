9d24ce04a65ced3cbb5d3fd07fe93288eeee5400
StupidDan <danielbee912@gmail.com>
2017-08-17 19:28:02 +0000

    Changed the logo to setImage to setThumbnail

--- commands/about.js
+++ commands/about.js
@@ -3,7 +3,7 @@ const embed = new Discord.RichEmbed();
 exports.run = function(client, message, args) {
   embed.setTitle("__About__")
     .setDescription("**Robot Moderation to a bot coded by Dan#3769 and Mr Robot#8472, made for the Mr Robot Community Discord Server.**")
-    .setImage("https://cdn.discordapp.com/attachments/274843090076958721/346968526684422144/hypesquad-white.png")
+    .setThumbnail("https://cdn.discordapp.com/attachments/274843090076958721/346968526684422144/hypesquad-white.png")
   message.channel.sendMessage({
     embed
   });
