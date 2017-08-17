37c3efc16e0d75afa961e34c4d6ccce1ad7e106b
StupidDan <danielbee912@gmail.com>
2017-08-17 15:10:12 +0100

    New command system

---
+++ commands/about.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const embed = new Discord.RichEmbed();
+exports.run = function(client, message, args) {
+  embed.setTitle("__About__")
+    .setDescription("**Robot Moderation to a bot coded by Dan#3769 and Mr Robot#8472, made for the Mr Robot Community Discord Server.**")
+    .setImage("https://cdn.discordapp.com/attachments/274843090076958721/346968526684422144/hypesquad-white.png")
+  message.channel.sendMessage({
+    embed
+  });
+}
