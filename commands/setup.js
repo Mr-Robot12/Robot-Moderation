b04598aeae8f221ae5cadb219bed9f7493df2009
StupidDan <danielbee912@gmail.com>
2017-08-18 12:33:34 +0100

    New permissions system

--- commands/setup.js
+++ commands/setup.js
@@ -1,4 +1,5 @@
 exports.run = function(client, message, args) {
+  if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.sendMessage("**✋ You don't have permission to use this command!**");
   var guild = message.guild;
   message.reply('A Staff role has been created!');
   guild.createRole({
