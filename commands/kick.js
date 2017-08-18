fb880746320dea8917d707aec1d0a0849529d3a1
StupidDan <danielbee912@gmail.com>
2017-08-18 12:32:36 +0100

    New permissions system

--- commands/kick.js
+++ commands/kick.js
@@ -1,4 +1,5 @@
 exports.run = function(client, message, args) {
+  if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.sendMessage("**✋ You don't have permission to use this command!**");
   let member = message.mentions.members.first();
   let reason = message.content.split(/\s+/g).slice(2).join(" ");
   member.kick(reason);
