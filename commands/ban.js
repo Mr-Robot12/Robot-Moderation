535837afe25897cde1faf923438bc34ab1906769
StupidDan <danielbee912@gmail.com>
2017-08-18 12:32:12 +0100

    New permissions system

--- commands/ban.js
+++ commands/ban.js
@@ -1,4 +1,5 @@
 exports.run = function(client, message, args) {
+  if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.sendMessage("**✋ You don't have permission to use this command!**");
   let member = message.mentions.members.first();
   let reason = message.content.split(/\s+/g).slice(2).join(" ");
   member.ban(reason);
