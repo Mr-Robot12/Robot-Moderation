abeab933cc19e16b8326202304f8cae05840b52d
StupidDan <danielbee912@gmail.com>
2017-08-18 12:31:03 +0100

    Added new permissions system (permlvl system currently not used)

--- app.js
+++ app.js
@@ -20,4 +20,17 @@ var reload = (message, cmd) => {
 };
 exports.reload = reload;
 
+client.elevation = function(message) {
+  let permlvl = 0;
+  let member_role = message.guild.roles.find('name', 'Member');
+  if (member_role && message.guild.roles.has(member_role.id)) permlvl = 0;
+  let mod_role = message.guild.roles.find('name', 'Moderator');
+  if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 2;
+  let admin_role = message.guild.roles.find('name', 'Administrator');
+  if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 3;
+  let owner_role = message.guild.roles.find('name', 'Ovners');
+  if (message.author.id === owner_role.id) permlvl = 4;
+  return permlvl;
+}
+
 client.login(settings.token);
