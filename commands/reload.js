217b08c18ecaebeeeb30f69e5f1cd1601d2a1d90
StupidDan <danielbee912@gmail.com>
2017-08-18 12:33:15 +0100

    New permissions system

--- commands/reload.js
+++ commands/reload.js
@@ -1,5 +1,6 @@
 const main = require('../app.js');
 exports.run = function(client, message, args) {
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.sendMessage("**✋ You don't have permission to use this command!**");
   let cmd = args.join(' ');
   main.reload(message, cmd);
 };
