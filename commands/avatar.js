1971b5e1881c6b4bc0bb99d50213c9a5c30b0070
StupidDan <danielbee912@gmail.com>
2017-08-18 11:13:33 +0100

    !avatar command fixed

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,3 +1,3 @@
 exports.run = function(client, message, args) {
-  message.reply(`your avatar link is ` + msg.author.avatarURL);
+  message.reply(`your avatar link is ` + message.author.avatarURL);
 }
