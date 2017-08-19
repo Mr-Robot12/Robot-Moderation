d85fe22c6757a0c7de94b1ba848a7cccffe0350a
StupidDan <danielbee912@gmail.com>
2017-08-19 12:46:26 +0100

    Fixed !announce permissions

--- commands/announce.js
+++ commands/announce.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const embed = new Discord.RichEmbed();
 exports.run = function(client, message, args) {
-  //   Permissions currently broken!    // if (message.author.permlvl > 2) return message.channel.sendMessage("**You don't have permission to use this command!**");
+  if (!message.member.hasPermission("MENTION_EVERYONE")) return message.channel.sendMessage("**✋ You don't have permission to use this command!**");
   let text = message.content.split(/\s+/g).slice(1).join(" ");
   console.log(text);
   embed.setTitle("__Announcement__")
