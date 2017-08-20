59320c1e7231398da162fcdc8f67accff77f49a3
StupidDan <danielbee912@gmail.com>
2017-08-20 11:37:19 +0100

    Fixed my shitty grammer

--- commands/github.js
+++ commands/github.js
@@ -3,7 +3,7 @@ const embed = new Discord.RichEmbed();
 
 exports.run = function(client, message, args) {
   embed.setTitle("__Robot Moderation Source Code__")
-    .setDescription("**Robot Moderation is open-source!\n The source code of over at https://github.com/Mr-Robot12/Robot-Moderation**")
+    .setDescription("**Robot Moderation is open-source!\n The source code is over at https://github.com/Mr-Robot12/Robot-Moderation**")
     .setThumbnail("https://assets-cdn.github.com/images/modules/logos_page/Octocat.png")
   message.channel.send({
     embed
