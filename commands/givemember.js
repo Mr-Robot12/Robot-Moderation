95b135c84d3092997c257ddbef49d31da7f395ab
StupidDan <danielbee912@gmail.com>
2017-08-25 18:27:23 +0100

    Added givemember command

---
+++ commands/givemember.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const embed = new Discord.RichEmbed();
+
+exports.run = function(client, message, args) {
+  if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.sendMessage("**✋ You don't have permission to use this command!**");
+  var guild = message.guild;
+  var member = message.mentions.members.first();
+  guild.member(message.mentions.members.first()).addRole('347322157606502422').catch(error => console.log(error));
+  embed.setTitle("__Member Role Given__")
+  .setDescription(`The Member Role has been given to ${member}.`)
+  message.channel.send({embed});
+}
