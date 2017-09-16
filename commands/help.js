8a634cdecc5c0c07570ee21afd26161794f30798
Dan <danielbee912@gmail.com>
2017-09-16 10:40:53 +0100

    2.0 update

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,19 @@ const embed = new Discord.RichEmbed();
 
 exports.run = function(client,message, args) {
   embed.setTitle("__Robot Moderation Help__")
-  .setDescription("**The prefix for Robot Moderation is `!`**\n **The list of commands are here: http://mrrobotcommunity.tk/robotmoderation.html**")
+  .setDescription("**This is the help menu for Robot Moderation.**")
   .setThumbnail("http://mrrobotcommunity.tk/assets/avatar.jpg")
-  message.channel.send({embed});
+  .addField("about", "Posts an embed explaining some information about Robot Moderation. `!about`")
+  .addField("announce", "Posts an embed with whatever text you want. `!announce [text]`")
+  .addField("avatar", "Posts an embed with your avatar URL. `!avatar`")
+  .addField("ban", "Bans a mentioned user. `!ban [user] [reason]`")
+  .addField("github", "Posts a GitHub Repository for Robot Moderation. `!github`")
+  .addField("help", "DMs you this embed. `!help`")
+  .addField("invite", "Posts an invite link too add Robot Moderation to your server. `!invite`")
+  .addField("kick", "Kicks a mentioned user. `!kick [user] [reason]`")
+  .addField("ping", "Pings your connection to the bot with a milliseconds value. `!ping`")
+  .addField("purge", "Purges a particular amount of messages. `!purge [amount of messages]`")
+  .addField("reload", "Reloads a command, without needing to restart the bot. `!reload [command]`")
+  message.author.send({embed});
+  message.channel.send("**I've PM'd you the help menu! :wink:**");
 }
