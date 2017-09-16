78e847a8776f209b75abca9cf6fbb12b89bec19c
Dan <danielbee912@gmail.com>
2017-09-16 10:39:26 +0100

    2.0 update

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,3 +1,8 @@
+const Discord = require('discord.js');
+
 exports.run = function(client, message, args) {
-  message.reply(`your avatar link is ` + message.author.avatarURL);
+ 	const embed = new Discord.RichEmbed();
+ 	embed.setTitle(`__Avatar for ${message.author.tag}__`)
+ 	.setImage(message.author.avatarURL)
+ 	message.channel.send({embed});
 }
