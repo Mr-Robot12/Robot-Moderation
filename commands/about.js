8c360dab6da54af26587c3a0e9b7ca9980e46113
Dan <danielbee912@gmail.com>
2017-09-16 11:46:04 +0100

    Fixed my shitty grammer

--- commands/about.js
+++ commands/about.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const embed = new Discord.RichEmbed();
 exports.run = function(client, message, args) {
   embed.setTitle("__About__")
-    .setDescription("**Robot Moderation to a bot coded by Dan.#2518 and Mr Robot#8472, made for the Mr Robot Community Discord Server.**")
+    .setDescription("**Robot Moderation is a bot coded by Dan.#2518 and Mr Robot#8472, made for the Mr Robot Community Discord Server.**")
     .setThumbnail("https://cdn.discordapp.com/attachments/274843090076958721/346968526684422144/hypesquad-white.png")
   message.channel.sendMessage({embed});
 }
