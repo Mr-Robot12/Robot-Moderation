30b0b0c6fe6bf57a8da85aedf759699a4ffd74e7
Dan <danielbee912@gmail.com>
2017-09-16 10:40:15 +0100

    2.0 update

--- commands/github.js
+++ commands/github.js
@@ -3,9 +3,8 @@ const embed = new Discord.RichEmbed();
 
 exports.run = function(client, message, args) {
   embed.setTitle("__Robot Moderation Source Code__")
+  .setColor("#ffa500")
     .setDescription("**Robot Moderation is open-source!\n The source code is over at https://github.com/Mr-Robot12/Robot-Moderation**")
     .setThumbnail("https://assets-cdn.github.com/images/modules/logos_page/Octocat.png")
-  message.channel.send({
-    embed
-  });
+  message.channel.send({embed});
 }
