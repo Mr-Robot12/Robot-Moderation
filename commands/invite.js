9e0a4b2f6cf630360444b1f4555caec24808496c
Dan <danielbee912@gmail.com>
2017-09-16 10:41:20 +0100

    2.0 update

--- commands/invite.js
+++ commands/invite.js
@@ -2,8 +2,7 @@ const Discord = require("discord.js");
 const embed = new Discord.RichEmbed();
 exports.run = function(client, message, args) {
   embed.setTitle("__To invite Robot Moderation to your server, use this link:__")
+  .setColor("#ffa500")
     .setDescription("**https://bot.discord.io/robotmoderation**")
-  message.channel.sendMessage({
-    embed
-  });
+  message.channel.sendMessage({embed});
 }
